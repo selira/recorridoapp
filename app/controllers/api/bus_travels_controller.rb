54c1127b61878be2217753330ee6d5ddabdf8231
selira <selira@uc.cl>
2021-09-18 13:25:14 -0300

    basic bus travel api call

--- app/controllers/api/bus_travels_controller.rb
+++ app/controllers/api/bus_travels_controller.rb
@@ -2,9 +2,9 @@ require 'http'
 
 class Api::BusTravelsController < ApplicationController
 
-  # GET /alerts or /alerts.json
-  def index
-    @alerts = Alert.all.order('id DESC')
+  def getBusTravels
+    bus_travel = "bus_travel%5Bdeparture_city_id%5D=9333&bus_travel%5Bdestination_city_id%5D=9360&bus_travel%5Bdeparture_date%5D=01-10-2021"
+    response = HTTP.basic_auth(:user => "recorrido", :pass => "recorrido").headers(:accept => "application/json", "Content-Type" => "application/x-www-form-urlencoded").post("https://demo.recorrido.cl/api/v2/es/bus_travels.json", :body => bus_travel).body.to_s
     render json: @alerts
   end
 
